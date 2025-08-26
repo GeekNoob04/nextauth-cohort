42af97e92cc75e83da4343885013ac76cb71e15e
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-27 03:27:19 +0530

    GoogleProvider

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,5 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 export const NEXT_AUTH = {
     providers: [
         CredentialsProvider({
@@ -23,6 +24,10 @@ export const NEXT_AUTH = {
                     email: "harshitbudhraja0@gmail.com",
                 };
             },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
