ed50d661b99cafe47795819dc0d393a99c31fe4c
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-27 03:43:02 +0530

    custom login page

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,6 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 export const NEXT_AUTH = {
     providers: [
         CredentialsProvider({
@@ -28,6 +29,10 @@ export const NEXT_AUTH = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || "",
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
